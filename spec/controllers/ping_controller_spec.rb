e775f688261c2d69370997836a3073968800fa3e
Jibran Kalia <jibran.kalia@gmail.com>
2020-03-19 15:23:37 -0500

    Adding test

--- spec/controllers/ping_controller_spec.rb
+++ spec/controllers/ping_controller_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Api::PingController, type: :controller do
   describe "#index" do
-    it "should get index" do
+    it "should return 200" do
       get :index
-      assert_response :success
+      expect(response.body).to eq('{"success":true}')
+      assert_response :ok
+    end
+
+    it "should return correct body" do
+      get :index
+      expect(response.body).to eq('{"success":true}')
     end
   end
 end
