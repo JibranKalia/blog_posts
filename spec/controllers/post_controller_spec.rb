6a9f8a0574dcd0854df3d504ce8f233d95d170be
Jibran Kalia <jibran.kalia@gmail.com>
2020-03-19 19:03:04 -0500

    Final tests

--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -2,24 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Api::PostsController, type: :controller do
   describe "#index" do
-    describe "errors" do
-      it "should return 400 and error message when tags missing" do
-        get :index
-        expect(response.body).to eq('{"error":"Tags parameter is required"}')
-        assert_response :bad_request
-      end
+    before(:each) do
+      stub_request(:get, "https://hatchways.io/api/assessment/blog/posts?tag=history")
+        .to_return( status: 200, body: '{"posts":[{"author":"Zackery Turner","popularity":0.68}]}', headers: {})
+    end
 
-      it "should return 400 when direction incorrect" do
-        get :index, params: { tags: "history", direction: "hi", format: :json }
-        expect(response.body).to eq('{"error":"direction parameter is invalid"}')
-        assert_response :bad_request
-      end
+    it "returns correct posts" do
+      get :index, params: { tags: "history", direction: "desc", sortBy: "popularity", format: :json }
+      expect(response.body).to eq('{"posts":[{"author":"Zackery Turner","popularity":0.68}]}')
+      assert_response :ok
+    end
 
-      it "should return 400 when sortBy incorrect" do
-        get :index, params: { tags: "history", sortBy: "hi", format: :json }
-        expect(response.body).to eq('{"error":"sortBy parameter is invalid"}')
-        assert_response :bad_request
-      end
+    it "should return 400 and error message when tags missing" do
+      get :index
+      expect(response.body).to eq('{"error":"Tags parameter is required"}')
+      assert_response :bad_request
+    end
+
+    it "should return 400 when direction incorrect" do
+      get :index, params: { tags: "history", direction: "hi", format: :json }
+      expect(response.body).to eq('{"error":"direction parameter is invalid"}')
+      assert_response :bad_request
+    end
+
+    it "should return 400 when sortBy incorrect" do
+      get :index, params: { tags: "history", sortBy: "hi", format: :json }
+      expect(response.body).to eq('{"error":"sortBy parameter is invalid"}')
+      assert_response :bad_request
     end
   end
 end
